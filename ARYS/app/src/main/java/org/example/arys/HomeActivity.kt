c8e6ed4622554b37a4301877e04474a8a4162d3e
emirhanaydin <emirhanaydin95@gmail.com>
2019-05-27 03:51:06 +0300

    Request multiple permissions

--- ARYS/app/src/main/java/org/example/arys/HomeActivity.kt
+++ ARYS/app/src/main/java/org/example/arys/HomeActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
@@ -14,8 +13,13 @@ import kotlinx.android.synthetic.main.activity_home.*
 class HomeActivity : AppCompatActivity() {
 
     companion object {
-        const val PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 0
+        const val REQUEST_CODE_ALL_PERMISSIONS = 0
     }
+
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,47 +40,47 @@ class HomeActivity : AppCompatActivity() {
         finish()
     }
 
+    private fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestPermissions() {
-        // Here, thisActivity is the current activity
-        when (ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )) {
-            PackageManager.PERMISSION_GRANTED -> {
-                // Permission has already been granted
-            }
-            else -> {
-                // Permission is not granted
-                // Should we show an explanation?
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    )
-                ) {
-                    // Show an explanation to the user *asynchronously* -- don't block
-                    // this thread waiting for the user's response! After the user
-                    // sees the explanation, try again to request the permission.
-                    onLocationPermissionDenied()
-                }
+        val pendingPermissions = permissions.distinctBy { s -> !hasPermission(s) }.toTypedArray()
 
-                // Request the permission.
-                ActivityCompat.requestPermissions(
+        pendingPermissions.forEach {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
                     this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+                    it
                 )
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                onLocationPermissionDenied()
             }
         }
+
+
+        // Request the permission.
+        ActivityCompat.requestPermissions(
+            this,
+            pendingPermissions,
+            REQUEST_CODE_ALL_PERMISSIONS
+        )
+
+        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == PERMISSION_REQUEST_ACCESS_FINE_LOCATION && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+        if (requestCode == REQUEST_CODE_ALL_PERMISSIONS
+            && grantResults.any { i -> i == PackageManager.PERMISSION_DENIED }
+        ) {
             onLocationPermissionDenied()
         }
     }
